f82971200dc85afa6690b0d087dc9d72224555cb
Châu Minh Thiện <minhthien1305@gmail.com>
2018-11-04 20:41:08 +0700

    fix url base

--- FrontendPublic/src/config/constants.js
+++ FrontendPublic/src/config/constants.js
@@ -3,4 +3,6 @@ export const KEY_SESSION      = 'WorkManagerSession';
 
 export const API_KEY          = 'FgQUvwaKRiXATWCFmhzHTA5iVCyvDRr6T0ntjBdhjOtA112';
 
-export const URL_BASE         = 'http://localhost:9100';
+// export const URL_BASE         = 'http://localhost:9100';
+export const URL_BASE         = 'https://work-s2.itfsoft.com';
+
